bc7acc3a2de2bc205ce03271a540fcbffcc98db1
jawwad22 <mjawwadiqbal22@gmail.com>
2018-11-11 00:38:13 -0800

    Add files via upload

---
+++ ParseTree/ParseTree/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParseTree
+{
+    class Node
+    {
+        public string Value;
+        public Node Left;
+        public Node Right;
+        public Node(string value)
+        {
+            this.Value = value;
+        }
+        public Node(string value, Node left, Node right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+}
---
+++ ParseTree/ParseTree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string elementAsString = "A 2 ^ 2 A * B * - B 2 ^ + A B - / 2 ^";
+            string[] elements = elementAsString.Split(' ');
+
+            Stack<Node> stack = new Stack<Node>();
+
+            foreach (string element in elements)
+            {
+                if (IsOperator(element))
+                {
+                    Node rightOperand = stack.Pop();
+                    Node leftOperand = stack.Pop();
+                    stack.Push(new Node(element, leftOperand, rightOperand));
+                }
+                else {
+                    Node n = new Node(element);
+                    stack.Push(n);
+                }
+            }
+        }
+       
+
+        private static bool IsOperator(string element)
+        {
+            switch (element)
+            {
+                case "*":
+                case "/":
+                case "^":
+                case "+":
+                case "-":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
